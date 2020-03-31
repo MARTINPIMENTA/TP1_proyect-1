094d64df2b6f29adce02d23c155226a025551a66
Martin Federico Pimenta Smurra <martin.pimenta@mercadolibre.com>
2020-03-31 20:27:02 -0300

    agregue funcion de ingreso integrante

--- Classes/main.java
+++ Classes/main.java
@@ -2,12 +2,26 @@ package Classes;
 
 import java.util.Scanner;
 
-public class main {
+public class test {
 
-	public static void main(String args) {
-		final String NOMBRE_GRUPO1 = "GRUPO YUMA";
+    public static void main(String args) {
+        final String NOMBRE_GRUPO1 = "GRUPO YUMA";
 
-		Grupo g1 = new Grupo(NOMBRE_GRUPO1);
-	}
+        Grupo g1 = Grupo(NOMBRE_GRUPO1);
+        ingresoIntegrante(g1, "Adrian Tamashiro");
+        ingresoIntegrante(g1, "Mariam Sleiman");
+        ingresoIntegrante(g1, "Ignacio Gaillard");
+        ingresoIntegrante(g1, "Martin Pimenta");
+        ingresoIntegrante(g1, "Carlos Perez");
 
+    }
+
+    public void ingresoIntegrante(Grupo grupo, String nombre) {
+        String integrante = grupo.buscarIntegrante(nombre);
+        if (integrante == null) {
+            grupo.agregarIntegrante(nombre);
+            System.out.println(nombre + " fue asignado al grupo " + grupo.getNombre());
+        } 
+        
+    }
 }
